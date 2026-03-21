bfcfd65a47159689f5da92424dd0b635dd253595
Tasos Giannakakis <agiannakakis@deloitte.gr>
2026-03-21 14:04:39 +0200

    feat: added movie interfaces and models

---
+++ src/app/core/models/movie.model.ts
@@ -0,0 +1,40 @@
+export interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  vote_average: number;
+  overview: string;
+  release_date: string;
+}
+
+export interface MovieDetails extends Movie {
+  budget: number;
+  revenue: number;
+  vote_count: number;
+  spoken_languages: SpokenLanguage[];
+}
+
+export interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface MovieSearchResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface GuestSession {
+  success: boolean;
+  guest_session_id: string;
+  expires_at: string;
+}
+
+export interface RatingResponse {
+  success: boolean;
+  status_code: number;
+  status_message: string;
+}
